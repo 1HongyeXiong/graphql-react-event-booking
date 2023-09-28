1675088c7a0afef40b104fa04127a3b34645262d
Hongye <hongye.xiong@outlook.com>
2023-09-28 16:18:54 -0700

    Use the mongodb as the database(create model)

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const  graphqlHttp   = require('express-graphql').graphqlHTTP;
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event')
 
 
 
 
 const app = express()
-const events = [];
+
 
 app.use(bodyParser.json());
 
@@ -50,22 +53,49 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return events;
+            return Event.find().then(events => {
+                return events.map(event => {
+                    return { ...event._doc, _id: event._doc._id.toString()};
+                });
+            }).catch(err => {
+                throw err;
+            });
         },
-        createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+        createEvent: args => {
+            // const event = {
+            //     _id: Math.random().toString(),
+            //     title: args.eventInput.title,
+            //     description: args.eventInput.description,
+            //     price: +args.eventInput.price,
+            //     date: args.eventInput.date
+            // };
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: args.eventInput.date
-            };
-            events.push(event);
-            return event;
+                date: new Date(args.eventInput.date)
+
+            });
+            return event.save().then(result => {
+                console.log(result);
+                return { ...result._doc }
+            }).catch(err => {
+                console.log(err);
+                throw err;
+            });
+            
         }
     },
     graphiql:true
 }) );
+
+
+// Database connection
+const URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphql-jerry-wfbgv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+mongoose.connect(URI, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+    console.log(`Connected to Mongo DB:${process.env.MONGO_DB}`);
+});
 
 
 
